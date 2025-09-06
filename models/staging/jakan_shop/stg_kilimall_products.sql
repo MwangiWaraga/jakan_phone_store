8605ab631638aebaa65566c65028b2b1b4d86f5f
Jackson Mwangi <jakanphonestore@gmail.com>
2025-09-07 01:16:54 +0300

    Fixed the stg_kilimall_prodcuts model JOIN; to only return the most recent records based on the column updated

--- models/staging/jakan_shop/stg_kilimall_products.sql
+++ models/staging/jakan_shop/stg_kilimall_products.sql
@@ -2,6 +2,12 @@
     materialized = 'ephemeral' if target.name == 'prod' else 'view'
 ) }}
 
+WITH recent as (
+    SELECT
+        MAX(updated_at) AS max_updated_at
+    FROM {{source('jakan_phone_store', 'kilimall_products_live_gsheets')}}
+)
+
 SELECT
   updated_at,
   LOWER(TRIM(store_name)) AS store_name,
@@ -10,6 +16,8 @@ SELECT
   listing_id,
   SAFE_CAST(regexp_replace(regexp_replace(price, 'KSh', ''), ',', '') AS numeric) AS price
 FROM {{source('jakan_phone_store', 'kilimall_products_live_gsheets')}}
+CROSS JOIN recent
 WHERE 
     product_title IS NOT NULL
     AND listing_id IS NOT NULL
+    AND updated_at = recent.max_updated_at
