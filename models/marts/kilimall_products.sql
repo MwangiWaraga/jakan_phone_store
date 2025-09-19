5ec8149f71cab36427c2e26a12c4688600f2e4e8
Jackson Mwangi <jackwaraga@gmail.com.com>
2025-09-19 14:41:17 +0300

    Revamping this project and using a different approach for kilimall products and stage oraimo products model

---
+++ models/marts/kilimall_products.sql
@@ -0,0 +1,16 @@
+with raw as (
+    select 
+      listing_id,
+      sku_id,
+      initcap(trim(split(title, ' ')[SAFE_OFFSET(0)])) as brand,
+      initcap(trim(substr(product_title, length(split(product_title, ' ')[safe_offset(0)]) + 2))) as product_title,
+      market_reference_price as market_price,
+      selling_price,
+      non_fbk_inventory as stock,
+      case   
+        when status = "ACTIVE" then 1
+        else 0
+      end as is_active,
+      updated_at_ts
+    from {{source('jakan_phone_store', 'kilimall_products_raw')}}
+)
